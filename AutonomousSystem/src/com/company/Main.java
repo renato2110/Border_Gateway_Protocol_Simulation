951739c1180d9eccf901b466e7845d02b5d8dca5
renato2110 <renato.mainieri2110@gmail.com>
2017-10-21 01:32:41 -0600

    Progreso, al suave

--- AutonomousSystem/src/com/company/Main.java
+++ AutonomousSystem/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.StringTokenizer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        RoutingTable routingTable = new RoutingTable("AS9");
+        routingTable.receiveUpdate("AS1*192.168.0.0:AS1-AS2-AS3,10.0.0.0:AS1-AS3");
+        routingTable.sendUpdates();
+
     }
 }
--- AutonomousSystem/src/com/company/RoutingTable.java
+++ AutonomousSystem/src/com/company/RoutingTable.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 /**
  * Created by Renato & Vladimir on 21/10/2017.
@@ -12,5 +14,46 @@ public class RoutingTable {
 
     public RoutingTable(String id) {
         this.id = id;
+        this.routes = new HashMap<>();
+    }
+
+    public void addSubnet(String route) {
+        StringTokenizer tokensRoute = new StringTokenizer(route, ":");
+        String subnet = tokensRoute.nextToken();
+        String path = this.id + "-" + tokensRoute.nextToken();
+
+        routes.put(subnet, path);
+    }
+
+    public void receiveUpdate(String packet) {
+        StringTokenizer tokensPacket = new StringTokenizer(packet, "*");
+        tokensPacket.nextToken();
+        String transmitterRoutes = tokensPacket.nextToken();
+        StringTokenizer tokensTransmitterRoutes = new StringTokenizer(transmitterRoutes, ",");
+        while (tokensTransmitterRoutes.hasMoreTokens()) {
+            String updatedRoute = tokensTransmitterRoutes.nextToken();
+            this.addSubnet(updatedRoute);
+        }
+    }
+
+    public void sendUpdates() {
+        String packet = this.id+"*";
+        int i = 1;
+        for (Map.Entry<String, String> entry : routes.entrySet()) {
+            packet += entry.getKey()+":"+entry.getValue();
+            if(i<routes.size()){
+                packet += ",";
+                i++;
+            }
+        }
+        // Uso del socket
+    }
+
+    public void updateRoute(String route) {
+        // pregunta si una ruta es mejor, y actualiza
+    }
+
+    public void showRoutes() {
+        // Imprimir cada ruta del hashMap
     }
 }
