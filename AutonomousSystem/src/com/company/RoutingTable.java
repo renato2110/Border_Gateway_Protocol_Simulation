80eee96514725bfe0f31b6eab91f9bfc32b62ef6
renato2110 <renato.mainieri2110@gmail.com>
2017-10-21 00:32:39 -0600

    Test

--- AutonomousSystem/src/com/company/RoutingTable.java
+++ AutonomousSystem/src/com/company/RoutingTable.java
@@ -1,7 +1,16 @@
 package com.company;
 
+import java.util.HashMap;
+
 /**
  * Created by Renato & Vladimir on 21/10/2017.
  */
 public class RoutingTable {
+
+    private String id;
+    private HashMap<String, String> routes;
+
+    public RoutingTable(String id) {
+        this.id = id;
+    }
 }
