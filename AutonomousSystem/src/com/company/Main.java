506e1d688c1a4dcf10c6a85748c689ca33305d14
renato2110 <renato.mainieri2110@gmail.com>
2017-10-22 01:30:21 -0600

    Progreso, al suave

--- AutonomousSystem/src/com/company/Main.java
+++ AutonomousSystem/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         RoutingTable routingTable = new RoutingTable("AS9");
-        routingTable.receiveUpdate("AS1*192.168.0.0:AS1-AS2-AS3,10.0.0.0:AS1-AS3,192.168.0.0:AS2-AS8,192.168.0.0:AS2-AS8");
+        routingTable.receiveUpdate("AS1*192.168.0.0:AS1-AS2-AS3,10.0.0.0:AS1-AS3,192.168.0.0:AS2-AS8,192.168.0.0:AS2-AS8,10.0.0.0:A4-AS6");
         routingTable.showRoutes();
         System.out.println("\n"+routingTable.getUpdatePackage());
 
